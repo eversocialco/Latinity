c9a215430b10ad47e47f92a843c9cf56d6da542b
William Serna <wsernalaverde@gmail.com>
2017-04-17 16:52:15 -0500

    Upload Latinity
    first upload

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+var options = {"particles":{"number":{"value":90,"density":{"enable":true,"value_area":552.4033491425909}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":3},"image":{"src":"img/github.svg","width":70,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1.5782952832645452,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":false};
+
+var optionsMenu = {"particles":{"number":{"value":90,"density":{"enable":true,"value_area":552.4033491425909}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":3},"image":{"src":"img/github.svg","width":70,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1.5782952832645452,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":false};
+
+
+$(function() {
+  $(document).ready(function(){
+      loadAll(function(){
+        particlesJS("bgspace", options);
+      });
+  });
+
+});
+
+function printElements(){
+  $("#menu").load("http://everbrander.co/latinity/templates/header.html", function(){
+    loadContent();
+    particlesJS("bgspaceMenu", optionsMenu);
+    $(".button-collapse").sideNav();
+  });
+  $("#footer").load("http://everbrander.co/latinity/templates/footer.html", function(){
+    loadContent();
+  });
+}
+
+
+function loadContent(){
+  jQuery(document).ready(function() {
+    jQuery('.effect').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
+        offset: 100
+       });
+    jQuery('.menuFooter a[href*="#"]:not([href="#"])').click(function() {
+       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+       }
+    });
+
+  });
+}
+
+function loadAll(callback){
+  printElements();
+  loadContent();
+  callback();
+}
+  
